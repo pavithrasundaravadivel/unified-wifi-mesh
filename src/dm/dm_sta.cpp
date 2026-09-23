c02a68c8ecb094b3dc1529851abb12a66edb509a
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-09-23 07:50:32 +0000

    compilation

--- src/dm/dm_sta.cpp
+++ src/dm/dm_sta.cpp
@@ -688,6 +688,11 @@ dm_sta_t::dm_sta_t(const dm_sta_t& sta)
     memcpy(&m_sta_info, &sta.m_sta_info, sizeof(em_sta_info_t));
 }
 
+dm_sta_t::dm_sta_t()
+{
+    memset(&m_sta_info, 0, sizeof(em_sta_info_t));
+}
+
 dm_sta_t::~dm_sta_t()
 {
 }
