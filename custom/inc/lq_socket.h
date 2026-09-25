67df70a304cab6bb204d381b200f1c8c42105b3c
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-09-25 07:39:35 +0000

    compilation

--- custom/inc/lq_socket.h
+++ custom/inc/lq_socket.h
@@ -20,7 +20,7 @@
 #define LQ_IPC_MSG_SET_SCORE_PARAMS 11
 
 typedef char mac_addr_str_t[18]; /**< MAC address string type. */
-typedef struct {
+/*typedef struct {
     unsigned long cli_PacketsSent;
     unsigned long cli_PacketsReceived;
     unsigned long cli_RetransCount;
@@ -50,7 +50,7 @@ typedef struct {
     char dhcp_hostname[256];
     char dhcp_vendor_class[256];
     char dhcp_param_list[512];
-} wei_data_t;
+} wei_data_t;*/
 
 /*
  * LQ TLV — the entire datagram is a single TLV, no wrapper header.
